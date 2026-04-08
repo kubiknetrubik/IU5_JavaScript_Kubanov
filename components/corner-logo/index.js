e083f5a7a9e06a9030a4a44d8b649e9ca519ee94
sasku <serkub06@yandex.ru>
2026-04-08 10:33:02 +0300

    first

--- components/corner-logo/index.js
+++
@@ -1,9 +0,0 @@
-export class LogoComponent {
-    constructor(parent) {
-        this.parent = parent;
-    }
-
-    render() {
-        this.parent.insertAdjacentHTML('beforeend', '<div id="corner" class="corner-image" style="display: block;width: 33%;"><img src="assets/metron.png" alt="Логотип" style="width: 250px;"></div>');
-    }
-}
